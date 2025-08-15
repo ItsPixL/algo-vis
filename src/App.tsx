c80fcb5b3a716c97d354a553800d2da98e9148dd
Neer Sheth <sheth.neer27@gmail.com>
2025-08-15 19:02:30 +1000

    Setup all base files - ready to start building.

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-import React from "react";
+// src/App.tsx
 
+// Imports
+import { motion } from "motion/react";
+
+// App
 export const App = () => {
-  return <div>App</div>;
+  return (
+    <motion.div className="bg-slate-950 w-screen h-screen">
+      <motion.div>Hello</motion.div>
+    </motion.div>
+  );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
+// src/main.tsx
+
+// Imports
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import { App } from "./App.tsx";
 
+// Main
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
